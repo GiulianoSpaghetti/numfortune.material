7fffa4faebcb3a3c199f0af8c8e55872b869b924
Giulio Sorrentino <numerone@fastwebnet.it>
2025-03-03 21:00:03 +0100

    Ora l'avviatore della pagina prncipale del progetto è in avalonia

--- numfortune/Pages/InfoPage.axaml.cs
+++ numfortune/Pages/InfoPage.axaml.cs
@@ -1,13 +1,28 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
+using System;
 
 namespace numfortune.Pages;
 
 public partial class InfoPage : UserControl
 {
+
+    private static ILauncher? launcher = null;
+    private static readonly Uri HomePage = new Uri("https://github.com/GiulianoSpaghetti/numfortune.material");
     public InfoPage()
     {
         InitializeComponent();
+    }
+
+    public void OnSito_Click(Object sender, RoutedEventArgs e)
+    {
+        if (launcher == null)
+        {
+            launcher = TopLevel.GetTopLevel(this).Launcher;
+        }
+        launcher.LaunchUriAsync(HomePage);
     }
 }
