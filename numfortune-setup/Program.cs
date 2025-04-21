4ea4df925d6d4fcb3045017091d390093eeba31b
Giulio Sorrentino <numerone@fastwebnet.it>
2025-04-21 16:36:56 +0200

    aggiornato ad avalonia 11.2.8

--- numfortune-setup/Program.cs
+++ numfortune-setup/Program.cs
@@ -22,20 +22,20 @@ namespace numfortune_setup
                                     )
                         ),
                         new Dir(@"%ProgramMenu%",
-                         new ExeFileShortcut("numfortune.Avalonia", "[ProgramFiles64Folder]\\numfortune.Avalonia\\numfortune.Desktop.exe", "") { WorkingDirectory = "[INSTALLDIR]" }
+                         new ExeFileShortcut("numerone's fortune", "[ProgramFiles64Folder]\\numfortune.Avalonia\\numfortune.Desktop.exe", "") { WorkingDirectory = "[INSTALLDIR]" }
                       )
             );
 
             project.GUID = new Guid("90D24925-7418-48AD-9950-676C2F839B20");
-            project.Version = new Version("3.0");
+            project.Version = new Version("3.0.0.6");
             project.Platform = Platform.x64;
-            project.SourceBaseDir = "C:\\Users\\numer\\source\\repos\\Numfortune\\numfortune.Desktop\\bin\\Release\\net9.0-windows10.0.26100.0";
+            project.SourceBaseDir = "f:\\source\\Numfortune.material\\numfortune.Desktop\\bin\\Release\\net9.0-windows10.0.26100.0";
             project.LicenceFile = "LICENSE.rtf";
-            project.OutDir = "E:\\";
+            project.OutDir = "f:\\";
             project.ControlPanelInfo.Manufacturer = "Giulio Sorrentino";
             project.ControlPanelInfo.Name = "numerone's fortune in avalonia";
             project.ControlPanelInfo.HelpLink = "https://github.com/GiulianoSpaghetti/numfortune.material/issues";
-            project.Description = "Una semplice fortune app per windows 10 e 11";
+            project.Description = "Un fortune teller per windows 10 e 11 con cookie americani nel dialetto material di google di avalonia";
             project.BuildMsi();
         }
     }
